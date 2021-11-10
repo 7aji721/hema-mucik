4ebff0e4d9dc17143786ece5d455ffa47d7050ed
iam-farhang <92163585+iam-farhang@users.noreply.github.com>
2021-11-10 05:08:53 +0300

    Update slap.js

--- others/slap.js
+++ others/slap.js
@@ -21,7 +21,7 @@ module.exports = {
         .setTitle(user.username + " You have been slapped! ")
         .setDescription((user.toString() + " got slapped by " + message.author.toString()))
         .setImage(owo.url)
-        .setColor(`BLACK`)
+        .setColor(`#8F00FF`)
         .setURL(owo.url);
         message.channel.send(kissembed);
 

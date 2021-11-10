e898269800ab05c29e032fa59fb2793995c4ab72
iam-farhang <92163585+iam-farhang@users.noreply.github.com>
2021-11-10 05:04:58 +0300

    Update ping.js

--- others/ping.js
+++ others/ping.js
@@ -13,8 +13,8 @@ module.exports = {
   edesc: "Type this command to see how fast the Bot can response to your messages / commands inputs!",
   execute(message, args, client) {
     //react with approve emoji
-    message.react("<:emoji_4:822203026776391711>");
+    message.react("✅");
     //send the Ping embed
-    message.reply(new MessageEmbed().setColor("RANDOM").setTitle("📶 `" + client.ws.ping + "ms`"));
+    message.reply(new MessageEmbed().setColor("#8F00FF").setTitle("📶 `" + client.ws.ping + "ms`"));
   }
 }

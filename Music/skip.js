de269a4cc662c94d31e357bde746e1f1b0d8d7a5
iam-farhang <92163585+iam-farhang@users.noreply.github.com>
2021-11-12 04:12:33 +0300

    Update skip.js

--- Music/skip.js
+++ Music/skip.js
@@ -19,7 +19,7 @@ execute(message) {
     //if not in a guild retunr
     if (!message.guild) return;
     //react with approve emoji
-    message.react("<:emoji_4:822203026776391711>").catch(console.error);
+    message.react("#8F00F").catch(console.error);
     //get the queue
     const queue = message.client.queue.get(message.guild.id);
     //if no Queue return error
@@ -33,7 +33,7 @@ execute(message) {
     queue.connection.dispatcher.end();
     //send approve message
     queue.textChannel.send(
-      new MessageEmbed().setColor("BLACK").setAuthor(`${message.author.username} skipped the song.`, "")
+      new MessageEmbed().setColor("#8F00F").setAuthor(`${message.author.username} skipped the song.`, "")
     ).catch(console.error);
   }
 };

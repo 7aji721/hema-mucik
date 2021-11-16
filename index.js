e97ce1ae34534837b89a0383c05f51f0ae296293
iam-farhang <92163585+iam-farhang@users.noreply.github.com>
2021-11-17 01:48:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2110,3 +2110,33 @@ var channel = client.channels.cache.get('909115134859964427');
 });
 
 //
+
+
+const Discord = require("discord.js");
+const Yaxi = new Discord.Client();
+//////// playing or dnd  streaming ////////
+Yaxi.on("ready", () => {
+  console.log(
+    `Online In Servers`
+  );
+  let statuses = [
+  
+  `@help | dev bot.bokani.`,
+ 
+  ];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    Yaxi.user.setActivity(STREAMING, {      
+    type: "STREAMING", 
+    url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+///////////////////////
+Yaxi.on("ready", () => {
+    var join = Yaxi.channels.get("906333162555199488"); /// id voice
+    if (join) join.join();
+  });
+//////////////// tokn lera da bne 
+Yaxi.login("NODQxNzg4NjQ3MDE0NTMxMDcz.YJr2xQ.g9DmUqKYkOL27zWAKRIKViRXiuk");
+

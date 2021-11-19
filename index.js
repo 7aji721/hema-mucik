0070f92905816b3d06d4db72fa3d58dae40179b6
iam-farhang <92163585+iam-farhang@users.noreply.github.com>
2021-11-19 12:58:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2105,7 +2105,7 @@ client.on("message", message => {
 });
 
 client.on('ready', () => {
-var channel = client.channels.cache.get('909115134859964427');
+var channel = client.channels.cache.get('900476676453068800');
     if (channel) channel.join();
 });
 

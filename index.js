41d0dc9c23bdee708dd34a473b3b3835bff362e8
iam-farhang <92163585+iam-farhang@users.noreply.github.com>
2021-12-24 19:49:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login("OTA3NjY0NzY0OTM3ODM0NTA2.YYqerw.9NLPUX_w-Xz1GWGSrVmSyaiPZZY");
+client.login("");
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
